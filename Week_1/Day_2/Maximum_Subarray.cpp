f74d7d94a6bdb52b8425a95aea0096578eb796d3
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-02 13:47:23 +0530

    Add Maximum Subarray solution in Maximum_Subarray.cpp
    Implement the Maximum Subarray problem solution using Kadane's algorithm.

---
+++ Week_1/Day_2/Maximum_Subarray.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: Maximum Subarray
+LeetCode: 53
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
