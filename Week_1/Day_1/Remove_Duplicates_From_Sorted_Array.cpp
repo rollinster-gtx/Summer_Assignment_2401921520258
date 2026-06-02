122e5ec0cf7143b72653d221f517664159ace1c5
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-02 13:33:03 +0530

    Add solution for removing duplicates from sorted array
    Implemented a solution to remove duplicates from a sorted array using O(n) time and O(1) space complexity.

---
+++ Week_1/Day_1/Remove_Duplicates_From_Sorted_Array.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Remove Duplicates from Sorted Array
+LeetCode: 26
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+
+        int k = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
