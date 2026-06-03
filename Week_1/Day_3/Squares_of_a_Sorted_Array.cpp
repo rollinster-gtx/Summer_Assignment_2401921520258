c791047aa6f9f431906ddaa5b5a6ff5046603f90
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-03 20:14:24 +0530

    Add solution for Squares of a Sorted Array problem
    Implement solution for the 'Squares of a Sorted Array' problem from LeetCode, which returns a sorted array of squares of the input integers.

---
+++ Week_1/Day_3/Squares_of_a_Sorted_Array.cpp
@@ -0,0 +1,34 @@
+/*
+Problem: Squares of a Sorted Array
+LeetCode: 977
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        int left = 0, right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[pos] = leftSquare;
+                left++;
+            } else {
+                result[pos] = rightSquare;
+                right--;
+            }
+
+            pos--;
+        }
+
+        return result;
+    }
+};
