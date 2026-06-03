241f39e716e0e372bf5d72075affdc15f82b6970
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-03 20:20:49 +0530

    Add solution for Container With Most Water problem
    Implemented the solution for the 'Container With Most Water' problem on LeetCode.

---
+++ Week_1/Day_3/Container_With_Most_Water.cpp
@@ -0,0 +1,29 @@
+/*
+Problem: Container With Most Water
+LeetCode: 11
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = min(height[left], height[right]);
+            int area = width * h;
+
+            maxWater = max(maxWater, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxWater;
+    }
+};
