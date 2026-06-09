7aae1e9a14ce916d7380f61b2c78ef1f8498b217
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-10 01:14:24 +0530

    Add solution for longest substring problem
    Implemented a solution to find the length of the longest substring without repeating characters.

--- Week_2/Day_2/Longest_Substring_Without_Repeating_Characters.cpp
+++ Week_2/Day_2/Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,1 +1,31 @@
+/*
+Problem: Longest Substring Without Repeating Characters
+LeetCode: 3
 
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        vector<int> lastIndex(256, -1);
+
+        int left = 0;
+        int maxLen = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+
+            if(lastIndex[s[right]] >= left) {
+                left = lastIndex[s[right]] + 1;
+            }
+
+            lastIndex[s[right]] = right;
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
