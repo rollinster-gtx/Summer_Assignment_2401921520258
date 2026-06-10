fe41ba8c71fc4a97351b476d11db41e422d5b594
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-10 23:29:55 +0530

    Add isSubsequence function implementation
    Implement the isSubsequence function to check if string 's' is a subsequence of string 't'.

---
+++ Week_2/Day_3/Is_Subsequence.cpp
@@ -0,0 +1,26 @@
+/*
+Problem: Is Subsequence
+LeetCode: 392
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+
+        int i = 0, j = 0;
+
+        while(i < s.size() && j < t.size()) {
+
+            if(s[i] == t[j]) {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i == s.size();
+    }
+};
