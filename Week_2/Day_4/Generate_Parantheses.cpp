d8ab14c3faf83466de805a0bcf3d0a815515773c
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-13 23:31:03 +0530

    Add Generate Parentheses solution
    Implement a solution to generate all combinations of well-formed parentheses using recursion.

---
+++ Week_2/Day_4/Generate_Parantheses.cpp
@@ -0,0 +1,37 @@
+/*
+Problem: Generate Parentheses
+LeetCode: 22
+
+Time Complexity: O(4^n / sqrt(n))
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+
+    void solve(int open, int close, string curr,
+               vector<string>& ans, int n) {
+
+        if(curr.length() == 2 * n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        if(open < n) {
+            solve(open + 1, close, curr + "(", ans, n);
+        }
+
+        if(close < open) {
+            solve(open, close + 1, curr + ")", ans, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+
+        vector<string> ans;
+
+        solve(0, 0, "", ans, n);
+
+        return ans;
+    }
+};
