755c861bdc93f8b8d48ee0b034faf7171b0a67f0
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-06-17 20:19:59 +0530

    Add Next Greater Element I solution
    Implement solution for finding the next greater element for each element in nums1 based on nums2.

---
+++ Week_3/Day_3/Next_Greater_Element_I.cpp
@@ -0,0 +1,35 @@
+/*
+Problem: Next Greater Element I
+LeetCode: 496
+
+Time Complexity: O(n + m)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int, int> mp;
+        stack<int> st;
+
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            mp[nums2[i]] = st.empty() ? -1 : st.top();
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+
+        for(int num : nums1) {
+            ans.push_back(mp[num]);
+        }
+
+        return ans;
+    }
+};
